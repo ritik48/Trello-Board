3ed461b1d1a94b8bd155174f099a989eed484067
Ritik Raj <raj769417@gmail.com>
2024-07-29 15:48:37 +0530

    feat: added some constant urls and values

---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/trello";
+const NEXT_AUTH_SECRET = process.env.NEXT_AUTH_SECRET || "JEWFN934U504499349";
+
+export { MONGO_URL, NEXT_AUTH_SECRET };
