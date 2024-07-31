a86e5bbcc7c1bcc4d66debe01b8591ebb7244c6a
Ritik Raj <raj769417@gmail.com>
2024-07-31 14:32:21 +0530

    feat: display rearranged tasks

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -3,7 +3,8 @@ import { DisplayBoard } from "./components/DisplayBoard";
 import { authOptions } from "@/lib/authOptions";
 import { redirect } from "next/navigation";
 import { getTasks } from "@/server/services/getTasks";
-
+import { TasksProvider } from "@/components/contexts/useTasks";
+import { rearrangeTasks } from "@/lib/helper";
 
 export default async function Board() {
     const session = await getServerSession(authOptions);
@@ -11,19 +12,21 @@ export default async function Board() {
     if (!session?.user.username) {
         redirect("/");
     }
-
     const data = await getTasks();
 
     if (!data.success) {
         redirect("/");
     }
-    const { tasks } = data;
+    const { tasks, taskOrder } = data;
+    const updatedTasks = rearrangeTasks(tasks, taskOrder);
 
     return (
         <section className="flex-1 ">
             <div className="max-w-7xl mx-auto mt-16 flex flex-col gap-3">
                 <h1 className="text-left text-xl font-semibold">Your tasks</h1>
-                <DisplayBoard tasks={tasks} />
+                <TasksProvider>
+                    <DisplayBoard initialTasks={updatedTasks} />
+                </TasksProvider>
             </div>
         </section>
     );
