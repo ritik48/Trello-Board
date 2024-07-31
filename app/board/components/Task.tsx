1591b49b3b7c2a03a7f55c45755b8709c9183b99
Ritik Raj <raj769417@gmail.com>
2024-07-31 17:28:38 +0530

    feat: change style of task on drag

--- app/board/components/Task.tsx
+++ app/board/components/Task.tsx
@@ -12,17 +12,41 @@ export interface TaskProp {
 }
 
 export function Task({ id, title, status }: TaskProp) {
-    const { attributes, listeners, setNodeRef, transform, transition } =
-        useSortable({ id });
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        transition,
+        isDragging,
+    } = useSortable({
+        id,
+        data: {
+            type: "Task",
+            task: { id, title, status },
+        },
+    });
 
     const style = {
         transform: CSS.Transform.toString(transform),
         transition,
     };
 
+    if (isDragging) {
+        return (
+            <div
+                className="border rounded-md opacity-70 p-3 bg-zinc-500 text-zinc-100"
+                ref={setNodeRef}
+                style={style}
+            >
+                <p>{title}</p>
+            </div>
+        );
+    }
+
     return (
         <div
-            className="border rounded-md p-3 bg-zinc-800 text-secondary"
+            className="border border-zinc-500 rounded-md p-3 bg-zinc-800 text-secondary"
             ref={setNodeRef}
             style={style}
             {...attributes}
