d976a394822c994421fa3e5ad1ad5413e76360b0
Ritik Raj <raj769417@gmail.com>
2024-07-31 22:03:12 +0530

    feat: added route to delete a task

--- app/api/task/[id]/route.ts
+++ app/api/task/[id]/route.ts
@@ -1,5 +1,7 @@
 import { connectDb } from "@/lib/db";
+import { handleAuth } from "@/server/handleAuth";
 import { Task } from "@/server/models/task";
+import { User } from "@/server/models/user";
 import { NextRequest, NextResponse } from "next/server";
 
 // update taskids order, when task position is changed on drag and drop
@@ -25,6 +27,36 @@ export async function PATCH(
         return Response.json(
             { success: true, message: "Task updated" },
             { status: 201 }
+        );
+    } catch (error: any) {
+        return Response.json({
+            success: false,
+            message: error?.message || "Cannot process this request",
+        });
+    }
+}
+
+export async function DELETE(
+    req: NextRequest,
+    { params }: { params: { id?: string } }
+) {
+    try {
+        const { id: taskId } = params;
+
+        const userId = await handleAuth();
+
+        if (!taskId) {
+            throw new Error("Invalid payload");
+        }
+
+        await connectDb();
+
+        await Task.findOneAndDelete({ id: taskId });
+        await User.findByIdAndUpdate(userId, { $pull: { taskOrder: taskId } });
+
+        return Response.json(
+            { success: true, message: "Task deleted" },
+            { status: 200 }
         );
     } catch (error: any) {
         return Response.json({
