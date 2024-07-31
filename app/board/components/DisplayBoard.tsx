33e51ae5bfb46349c1de9a3dd48b71266429a8e6
Ritik Raj <raj769417@gmail.com>
2024-07-31 17:29:29 +0530

    feat: allow tasks to move to different coumn

--- app/board/components/DisplayBoard.tsx
+++ app/board/components/DisplayBoard.tsx
@@ -1,20 +1,81 @@
 "use client";
 
-import { useEffect, useMemo } from "react";
-import { Column } from "./Column";
-import { TaskProp } from "./Task";
+import { useEffect, useMemo, useState } from "react";
+import { Column, ColumnType } from "./Column";
+import { Task, TaskProp } from "./Task";
 
-import { DndContext, DragEndEvent } from "@dnd-kit/core";
+import {
+    DndContext,
+    DragEndEvent,
+    DragOverEvent,
+    PointerSensor,
+    useSensor,
+    useSensors,
+    DragOverlay,
+    DragStartEvent,
+} from "@dnd-kit/core";
+import { arrayMove, SortableContext } from "@dnd-kit/sortable";
 import { useTasks } from "@/components/contexts/useTasks";
+import { createPortal } from "react-dom";
+
+const cols: { id: string; column: ColumnType }[] = [
+    { id: "t", column: "todo" },
+    { id: "p", column: "progress" },
+    { id: "r", column: "review" },
+    { id: "f", column: "finished" },
+];
 
 export function DisplayBoard({ initialTasks }: { initialTasks: TaskProp[] }) {
     const { setTasks, changeOrder, tasks } = useTasks();
 
-    function handleDragEnd(event: DragEndEvent) {
+    const [activeTask, setActiveTask] = useState<TaskProp | null>(null);
+
+    async function handleDragEnd(event: DragEndEvent) {
+        // const { active, over } = event;
+        // console.log("inside odrag end");
+        // if (!over) return;
+        // if (active.id === over.id) return;
+        // await changeOrder(active, over);
+    }
+    function handleDragOver(event: DragOverEvent) {
         const { active, over } = event;
 
-        if (active.id !== over?.id) {
-            changeOrder(active, over);
+        if (!over) return;
+        if (active.id === over.id) return;
+        
+        if (
+            active.data.current?.type === "Task" &&
+            over.data.current?.type === "Task"
+        ) {
+            setTasks((prevTasks) => {
+                const oldIndex = prevTasks.findIndex((t) => t.id === active.id);
+                const newIndex = prevTasks.findIndex((t) => t.id === over?.id);
+
+                prevTasks[oldIndex].status = prevTasks[newIndex].status;
+
+                return arrayMove(prevTasks, oldIndex, newIndex);
+            });
+            return;
+        }
+
+        if (
+            active.data.current?.type === "Task" &&
+            over.data.current?.type === "Column"
+        ) {
+            setTasks((prevTasks) => {
+                const copy = [...prevTasks];
+                const oldIndex = prevTasks.findIndex((t) => t.id === active.id);
+
+                copy[oldIndex].status = over.data.current?.column.column;
+
+                return copy;
+            });
+        }
+    }
+    function onDragStart(event: DragStartEvent) {
+        if (event.active.data.current?.type === "Task") {
+            setActiveTask(event.active.data.current?.task);
+            return;
         }
     }
 
@@ -22,13 +83,46 @@ export function DisplayBoard({ initialTasks }: { initialTasks: TaskProp[] }) {
         setTasks(initialTasks);
     }, [initialTasks, setTasks]);
 
+    const columnIds = useMemo(() => {
+        return cols.map((c) => c.id);
+    }, [cols]);
+
+    const sensors = useSensors(
+        useSensor(PointerSensor, {
+            activationConstraint: { distance: 10 },
+        })
+    );
+
     return (
-        <div className="grid grid-cols-4 gap-7">
-            <DndContext onDragEnd={handleDragEnd}>
-                <Column tasks={tasks} column={"todo"} />
-                <Column tasks={tasks} column={"progress"} />
-                <Column tasks={tasks} column={"review"} />
-                <Column tasks={tasks} column={"finished"} />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 px-3 mx-2">
+            <DndContext
+                sensors={sensors}
+                onDragOver={handleDragOver}
+                onDragEnd={handleDragEnd}
+                onDragStart={onDragStart}
+            >
+                <SortableContext items={columnIds}>
+                    {cols.map((c) => (
+                        <Column
+                            tasks={tasks}
+                            column={c.column}
+                            key={c.id}
+                            id={c.id}
+                        />
+                    ))}
+                </SortableContext>
+                {createPortal(
+                    <DragOverlay>
+                        {activeTask && (
+                            <Task
+                                id={activeTask.id}
+                                status={activeTask.status}
+                                title={activeTask.title}
+                            />
+                        )}
+                    </DragOverlay>,
+                    document.body
+                )}
             </DndContext>
         </div>
     );
