dcc20cb5548eed3d1f919ec35f71410030c0d9f8
Ritik Raj <raj769417@gmail.com>
2024-07-31 14:31:07 +0530

    feat: added route to update task order

--- app/api/task/route.ts
+++ app/api/task/route.ts
@@ -1,7 +1,7 @@
-import { taskSchema } from "@/app/board/validations";
 import { connectDb } from "@/lib/db";
 import { handleAuth } from "@/server/handleAuth";
 import { Task } from "@/server/models/task";
+import { User } from "@/server/models/user";
 
 export async function POST(req: Request, res: Response) {
     try {
@@ -12,7 +12,11 @@ export async function POST(req: Request, res: Response) {
         await connectDb();
 
         const task = await Task.create({ ...taskData, user: userId });
-        console.log("task = ", task);
+
+        await User.findByIdAndUpdate(userId, {
+            $push: { taskOrder: taskData.id },
+        });
+
         return Response.json(
             { success: true, message: "Task addedd", task },
             { status: 201 }
@@ -34,6 +38,29 @@ export async function GET(req: Request, res: Response) {
         const tasks = await Task.find({ user: userId });
 
         return Response.json({ success: true, tasks }, { status: 200 });
+    } catch (error: any) {
+        return Response.json({
+            success: false,
+            message: error?.message || "Cannot process this request",
+        });
+    }
+}
+
+// update taskids order, when task position is changed on drag and drop
+export async function PATCH(req: Request, res: Response) {
+    try {
+        const userId = await handleAuth();
+
+        const taskData = await req.json();
+
+        await connectDb();
+
+        await User.findOneAndUpdate(
+            { _id: userId },
+            { taskOrder: taskData.taskIds }
+        );
+
+        return Response.json({ success: true }, { status: 201 });
     } catch (error: any) {
         return Response.json({
             success: false,
