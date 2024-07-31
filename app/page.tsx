4983cdb05a29c42b54c9ac44cd8592a3c51a19be
Ritik Raj <raj769417@gmail.com>
2024-07-31 19:19:39 +0530

    refactor: minor style change

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,17 @@ export default function Home() {
         <section className="flex-1 flex items-center">
             <div className="max-w-7xl mx-auto -mt-60">
                 <div className="flex flex-col justify-center items-center">
-                    <h1 className="text-5xl font-bold">
+                    <h1 className="text-3xl sm:text-5xl font-bold text-secondary text-center">
                         Organize Everything, Effortlessly
                     </h1>
-                    <p className="mt-3 text-sm sm:text-xl text-muted-foreground md:mt-5 md:max-w-3xl text-center">
+                    <p className="mt-3 sm:w-auto w-[90%] text-base sm:text-xl text-muted-foreground md:mt-5 md:max-w-3xl text-center">
                         Drag, drop, and done. Streamline your workflow with our
                         intuitive task management platform.
                     </p>
                     <div className="mt-5">
                         <Link
                             href={"/"}
-                            className="text-lg hover:bg-zinc-700 rounded-md bg-primary text-secondary px-6 py-2 flex justify-center items-center gap-2 hover:gap-6 transition-all duration-300"
+                            className="text-lg bg-zinc-700 shadow-sm shadow-zinc-700 rounded-md bg-primary text-secondary px-6 py-2 flex justify-center items-center gap-2 hover:gap-6 transition-all duration-300"
                         >
                             <span>Get Started</span>
                             <VscArrowRight />
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,44 +2,48 @@ import { authOptions } from "@/lib/authOptions";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { Logout } from "./Logout";
+import { HamMenu } from "./HamMenu";
 
 export async function Navbar() {
     const session = await getServerSession(authOptions);
-    console.log(session);
     const isAuthenticated = !!session?.user?.username;
     return (
-        <nav className="h-[50px] content-center">
+        <nav className="h-[50px] content-center py-2">
             <div className="max-w-7xl mx-auto">
                 <div className="flex justify-between items-center px-2">
                     <div className="flex items-center gap-16">
-                        <span className="text-xl font-bold">Trello B.</span>
-                        <Link
-                            href={"/board"}
-                            className="text-sm text-muted-foreground"
-                        >
-                            Tasks
-                        </Link>
+                        <span className="text-xl font-bold text-secondary">
+                            Trello B.
+                        </span>
+                        {isAuthenticated && (
+                            <Link
+                                href={"/board"}
+                                className="text-sm text-zinc-400 hover:underline"
+                            >
+                                Tasks
+                            </Link>
+                        )}
                     </div>
-                    <div className="flex items-center gap-4">
+                    <div className="sm:flex items-center gap-4 hidden">
                         {!isAuthenticated ? (
                             <>
                                 <Link
                                     href={"/"}
-                                    className="border px-4 py-1.5 rounded-md text-sm bg-primary text-secondary hover:opacity-70 transition-all duration-300"
+                                    className="border px-4 py-1.5 rounded-md text-sm bg-secondary text-primary hover:opacity-70 transition-all duration-300"
                                 >
                                     Login
                                 </Link>
                                 <Link
                                     href={"/"}
-                                    className="border px-4 py-1.5 rounded-md text-sm hover:bg-accent transition-all duration-300"
+                                    className="border px-4 py-1.5 rounded-md text-sm text-secondary hover:bg-zinc-800 transition-all duration-300"
                                 >
                                     Signup
                                 </Link>
                             </>
                         ) : (
                             <>
-                                <div className="flex items-center gap-2 border rounded-full px-4 py-0.5">
-                                    <p className="w-fit">
+                                <div className="flex items-center gap-2 border border-zinc-600 rounded-full px-4 py-0.5">
+                                    <p className="w-fit text-accent">
                                         {session?.user.username}
                                     </p>
 
@@ -48,6 +52,9 @@ export async function Navbar() {
                                 <Logout />
                             </>
                         )}
+                    </div>
+                    <div className="sm:hidden">
+                        <HamMenu isAuthenticated={isAuthenticated} />
                     </div>
                 </div>
             </div>
